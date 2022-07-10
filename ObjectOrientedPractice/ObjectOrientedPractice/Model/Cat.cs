c56399ec82bc8598722b1a1967f299c1d60bab7a
MaShunSolve <108853084+MaShunSolve@users.noreply.github.com>
2022-07-10 21:06:43 +0800

    新增建構式

--- ObjectOrientedPractice/ObjectOrientedPractice/Model/Cat.cs
+++ ObjectOrientedPractice/ObjectOrientedPractice/Model/Cat.cs
@@ -6,9 +6,26 @@ namespace ObjectOrientedPractice
     /// </summary>
     public class Cat
     {
+        private string name = "";
+        /// <summary>
+        /// 定義Cat類別建構式
+        /// </summary>
+        /// <param name="name"></param>
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+        /// <summary>
+        /// 重載
+        /// </summary>
+        public Cat()
+        {
+            this.name = "等待取名";
+        }
+
         public string Shout()
         {
-            return "喵";
+            return $"My name is {name} 喵~";
         }
     }
 }
