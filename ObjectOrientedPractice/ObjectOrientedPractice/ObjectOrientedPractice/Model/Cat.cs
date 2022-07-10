504a331f4ab4480b63601b1eb4c034f87c1c93ec
MaShunSolve <108853084+MaShunSolve@users.noreply.github.com>
2022-07-10 21:05:56 +0800

    建立建構式、使用方法重載

---
+++ ObjectOrientedPractice/ObjectOrientedPractice/ObjectOrientedPractice/Model/Cat.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ObjectOrientedPractice
+{
+    /// <summary>
+    /// Cat Class
+    /// </summary>
+    public class Cat
+    {
+        private string name = "";
+        /// <summary>
+        /// 定義Cat類別建構式
+        /// </summary>
+        /// <param name="name"></param>
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+        /// <summary>
+        /// 重載
+        /// </summary>
+        public Cat()
+        {
+            this.name = "等待取名";
+        }
+
+        public string Shout()
+        {
+            return $"My name is {name} 喵~";
+        }
+    }
+}
