6a4a161189d207ce1493789e7430d97885d528ee
MaShunSolve <108853084+MaShunSolve@users.noreply.github.com>
2022-07-10 20:42:29 +0800

    Create a cat class and new an cat instance

---
+++ ObjectOrientedPractice/ObjectOrientedPractice/Model/Cat.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ObjectOrientedPractice
+{
+    /// <summary>
+    /// Cat Class
+    /// </summary>
+    public class Cat
+    {
+        public string Shout()
+        {
+            return "喵";
+        }
+    }
+}
